0aaa0c2db46f9e9962afb2752c63fb9fe94af9bd
Aaron <aaronmadar2655@gmail.com>
2025-07-23 12:17:03 +0300

    crud-client => mongoDb

--- MANAGER/allimports.js
+++ MANAGER/allimports.js
@@ -1,8 +1,8 @@
-import  Riddle  from "../RIDDLE/Riddle.js"
-import {Player} from "../PLAYER/Player.js"
-import { riddlearray } from "../appback.js"
+import Riddle from "../RIDDLE/Riddle.js"
+import { Player } from "../PLAYER/Player.js"
 import { crudMenu } from "../appback.js"
 import { toSelectedOption } from "../appback.js"
+import { addRiddle , getAllRiddle, updateRiddle, deleteRiddle } from "../SERVERandDB/db.js"
 
-export {Riddle,Player,crudMenu,toSelectedOption,riddlearray}
+export { Riddle, Player, crudMenu, toSelectedOption, addRiddle , getAllRiddle, updateRiddle, deleteRiddle }
 
--- appback.js
+++ appback.js
@@ -1,13 +1,14 @@
 import readline from 'readline-sync';
-import * as ALL  from "./MANAGER/allimports.js"
+import * as ALL from "./MANAGER/allimports.js"
 import fs from "fs"
+import { log } from 'console';
 
 
-export function crudMenu(){
+export function crudMenu() {
 
-   console.log(`
+    console.log(`
 ╔════════════════════════════════════════════════════╗
-║                    MAIN MENU                      ║
+║                    MAIN MENU                       ║
 ╠════════════════════════════════════════════════════╣
 ║  Press 1 ➤  Play the game                         ║
 ║  Press 2 ➤  Create a new riddle                   ║
@@ -19,46 +20,44 @@ export function crudMenu(){
 `);
 
 
-    
+
 }
 
+export function toSelectedOption(choice, count) {
+
+    switch (choice) {
+
+        // case "1":
+        //     launchGame(count, player)
+        //     return false
 
 
-export function toSelectedOption(readli,count,player){
 
-    switch(readli){
-
-        case "1": 
-         launchGame(count,player)
-         return false
-         
-            
-    
-        case "2": 
-        createNewRiddle()
+        case "2":
+            createNewRiddle()
             break
-    
+
         case "3":
             readAllRiddles()
             break
-    
+
         case "4":
             updateRiddle()
-        break 
-    
+            break
+
         case "5":
-            deleteRiddle()
-        break
-    
+            delRiddle()
+            break
+
         case "6":
             viewLeaderboard()
-        break
-    
-        default :
-        console.log("Please write a correct choice !");        
-        
-        return true
-        
+            break
+
+        default:
+            console.log("Please write a correct choice !");
+
+            return true
+
 
 
 
@@ -66,95 +65,137 @@ export function toSelectedOption(readli,count,player){
 
 }
 
+export async function createNewRiddle() {
+    console.log("\n ~~~ ADD YOUR NEW RIDDLE ~~~\n");
+    let taski = readline.question("WRITE HERE THE TASK: ");
+    let answeri = readline.question("WRITE HERE THE ANSWER: ");
+    let leveli = readline.question("What is the level? (1 to 3): ");
 
-export let riddlearray = ALL.Riddle.getRiddleArray()
+    if (!taski || !answeri) {
+        console.log(`TASK OR ANSWER NOT VALID !`);
+        return true;
 
+    }
 
-function launchGame(count,player){
-    
+    // Vérifie que leveli est bien un chiffre entre 1 et 3
+    if (!["1", "2", "3"].includes(leveli)) {
+        console.log("Invalid level. Please enter 1, 2, or 3.");
+        return true;
+    }
 
-     for( let i = 0; i<riddlearray.length && count <3  ;i++ ){
-                let startime = Date.now()
-               let actualriddle =  riddlearray[i]
-               ALL.Riddle.Ask(count,actualriddle)
-                let endtime = Date.now()
-                player.recordTime(startime,endtime)
-                
-               
+    // Appel API
+    try {
+        let res = await fetch('http://localhost:3000/riddle', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                task: taski,
+                answer: answeri,
+                level: leveli
+            })
+        });
+
+        if (!res.ok) {
+            console.log(`banana`);
+
+            throw new Error(`Failed to create riddle: ${res.statusText}`);
         }
-    
-        console.log("Congratulation !!! ")
-        player.showStatis()
+
+        console.log("\nYour riddle has been added successfully!");
+
+    } catch (err) {
+        console.error("Error while adding riddle:", err.message);
+    }
+}
+
+export async function readAllRiddles() {
+    console.log(`\n ~~~ THE LIST OF ALL RIDDLES ~~~ \n`);
+
+    let res = await fetch('http://localhost:3000/allriddle', {
+        method: "GET",
+    })
+    if (!res.ok) {
+        throw new Error(`Server error: ${res.status}`);
+        return true;
+    }
+    const riddles = await res.json();
+    console.log(riddles);
+
+
+    if (riddles.length === 0) {
+        console.log("No riddles found.");
+        return false;
+    }
+
+    riddles.forEach((riddle, index) => {
+        console.log(`Riddle ${index + 1}`);
+        console.log(`-----------------------------`);
+        console.log(`Task   : ${riddle.task}`);
+        console.log(`Answer : ${riddle.answer}`);
+        console.log(`Level  : ${riddle.level}`);
+        console.log(`ID     : ${riddle._id}`);
+        console.log(``);
+    });
 
 }
 
+export async function updateRiddle(collection, id) {
+    try {
+        let targetid = (readline.question(`\nWHAT IS THE ID OF THE RIDDLE THAT YOU WANT UPDATED ? :  `))
 
+        if (!targetid) {
+            console.log(`Please enter a correct Id !`);
+            return true;
+        }
+        let task = readline.question("Enter the new task: ");
+        let answer = readline.question("Enter the new answer: ");
+        let level = readline.question("Enter the level (1 to 3): ")
 
+        let riddleup = { targetid, task, answer, level }
 
+        let res = await fetch("http://localhost:3000/updriddle", {
+            method: "POST",
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(riddleup)
+        })
+        if (!res.ok) {
+            console.log("Error server !");
+            return false
 
-export function createNewRiddle(){
-    console.log(" \n ~~~ ADD YOUR NEW RIDDLE ~~~\n")
-   let task = readline.question(`WRITE HERE THE TASK: `)
-    let answer = readline.question(`WRITE HERE THE ANSWER: `)
-  
-  let newriddle = new ALL.Riddle(task,answer)
-  
+        }
+        console.log("The Riddle is Updated succesfully !");
+        return false
 
-  console.log("\n Your riddle has added succesfully ! ")
-
-
+    }
+    catch (err) {
+        console.error("Error while updating riddle:", err.message);
+    }
 
 }
 
+export async function delRiddle() {
 
-
- function readAllRiddles(){
-
-        
-        console.log(`\n ~~~ THE LIST OF ALL RIDDLES ~~~ \n`)
-        console.log(riddlearray)
+    let targetid = readline.question(`\nWHAT IS THE ID OF THE RIDDLE THAT YOU WANT DELETED ? :  `)
+    if (!targetid) {
+        console.log('Please enter a valid Id !');
+        return;
     }
+    try {
 
-
-
-function updateRiddle(){
-    try{
-        let targetid = parseInt(readline.question(`\nWHAT IS THE ID OF THE RIDDLE THAT YOU WANT UPDATED ? :  `))
-        let index = riddlearray.findIndex(r => r.id === targetid);
-
-        if (!isNaN(targetid) && index !== -1) {
-            let task = readline.question("Enter the new task: ");
-            let answer = readline.question("Enter the new answer: ");
-
-            riddlearray[index].taskDescription = task;
-            riddlearray[index].correctAnswer = answer;
-            const updatedContent = riddlearray.map(r => JSON.stringify(r)).join('\n');
-            fs.writeFileSync('./RIDDLE/riddle.txt', updatedContent);
-
-            console.log(" Riddle updated successfully!");
-
+        let res = await fetch('http://localhost:3000/del', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ id: targetid })
+        })
+        if (!res.ok) {
+            console.log("Error server !");
+            return true
         }
-        else{
-            console.log(`WRONG ID !`)
-        }
+        console.log(" Riddle deleted successfully!");
+    } catch (e) {
+        console.log(`error : ${e}`);
     }
-    catch(error){
-        console.log(`Error ! ${error}`)
-    }
-
-    }
-
-
-function deleteRiddle(){
-    
-    let targetid = parseInt(readline.question(`\nWHAT IS THE ID OF THE RIDDLE THAT YOU WANT DELETED ? :  `))
-    let index = riddlearray.findIndex(r => r.id === targetid);
-    riddlearray.splice(index,1)
-    const updatedContent = riddlearray.map(r => JSON.stringify(r)).join('\n');
-    fs.writeFileSync('./RIDDLE/riddle.txt', updatedContent);
-
-    console.log(" Riddle deleted successfully!");
-
-
 
 }
